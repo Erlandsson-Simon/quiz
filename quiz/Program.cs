ff730064d4634b79fd17df18ef70836b2d2e9b17
Simon Erlandsson <erlandsson.simon@elev.ga.ntig.se>
2021-10-05 14:57:10 +0200

    Did it all :)

--- quiz/Program.cs
+++ quiz/Program.cs
@@ -1,1 +1,33 @@
 ﻿using System;
+using System.IO;
+
+string[] question = File.ReadAllLines(@"questions");
+string[] alternatives = File.ReadAllLines(@"alternatives");
+string[] rightAnswer = File.ReadAllLines(@"rightAnswer");
+
+int totalPoints = 0;
+
+string playerAnswer;
+
+for (int i = 0; i < question.Length; i++)
+{
+    Console.WriteLine(question[i]);
+
+    Console.WriteLine(alternatives[i]);
+
+    playerAnswer = Console.ReadLine();
+
+    if (playerAnswer == rightAnswer[i])
+    {
+        Console.WriteLine("Right answer");
+        totalPoints += 1;
+    }
+    else
+    {
+        Console.WriteLine("Wrong answer, right answer was " + rightAnswer[i]);
+    }
+
+}
+
+Console.WriteLine("Your total score is " + totalPoints);
+Console.ReadLine();
