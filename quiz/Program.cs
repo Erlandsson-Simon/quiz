ba7dbf761658123929f1a77b2ed4d3a52f8aef6c
Simon Erlandsson <erlandsson.simon@elev.ga.ntig.se>
2021-10-06 13:25:40 +0200

    skapa en restart knapp

--- quiz/Program.cs
+++ quiz/Program.cs
@@ -8,26 +8,46 @@ string[] rightAnswer = File.ReadAllLines(@"rightAnswer");
 int totalPoints = 0;
 
 string playerAnswer;
+string doGameRestart;
 
-for (int i = 0; i < question.Length; i++)
+Boolean startGame = false;
+Boolean killGame = false;
+
+while (killGame == false)
 {
-    Console.WriteLine(question[i]);
-
-    Console.WriteLine(alternatives[i]);
-
-    playerAnswer = Console.ReadLine();
-
-    if (playerAnswer == rightAnswer[i])
+    for (int i = 0; i < question.Length; i++)
     {
-        Console.WriteLine("Right answer");
-        totalPoints += 1;
-    }
-    else
-    {
-        Console.WriteLine("Wrong answer, right answer was " + rightAnswer[i]);
+        Console.WriteLine(question[i]);
+        Console.WriteLine(alternatives[i]);
+        playerAnswer = Console.ReadLine();
+
+        if (playerAnswer == rightAnswer[i])
+        {
+            Console.WriteLine("Right answer");
+            totalPoints += 1;
+        }
+        else
+        {
+            Console.WriteLine("Wrong answer, right answer was " + rightAnswer[i]);
+        }
     }
 
+    startGame = false;
+    while (startGame == false)
+    {
+        Console.WriteLine("Your total score is " + totalPoints);
+        Console.WriteLine("If you would like to play again press r");
+        doGameRestart = Console.ReadLine();
+        if (doGameRestart == "r" || doGameRestart == "R")
+        {
+            killGame = false;
+            startGame = true;
+        }
+        else
+        {
+            killGame = true;
+            startGame = true;
+        }
+    }
 }
-
-Console.WriteLine("Your total score is " + totalPoints);
 Console.ReadLine();
